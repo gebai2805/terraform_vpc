d92cf2f93ae45feb5b2a67bb693656f14fb001dc
gebai2805 <asthasingh2805@outlook.com>
2025-07-17 12:25:16 +0530

    Terraform script for master-worker node for ansible

---
+++ ansible.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_security_group" "terraformsg" {
+  tags = {
+    Name = "terraformsg"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "master" {
+  ami           = "ami-0f918f7e67a3323f0"
+  instance_type = "t2.micro"
+  key_name      = "geetha"
+  security_groups = [aws_security_group.terraformsg.name]
+  tags = {
+    Name = "master"
+  }
+}
+
+resource "aws_instance" "worker" {
+  ami           = "ami-0f918f7e67a3323f0"
+  instance_type = "t2.micro"
+  key_name      = "geetha"
+  security_groups = [aws_security_group.terraformsg.name]
+  tags = {
+    Name = "worker"
+  }
+}
+
+output "master_public_ip" {
+  value = aws_instance.master.public_ip
+}
+
+output "master_private_ip" {
+  value = aws_instance.master.private_ip
+}
+
+output "worker_public_ip" {
+  value = aws_instance.worker.public_ip
+}
+
+output "worker_private_ip" {
+  value = aws_instance.worker.private_ip
+}
